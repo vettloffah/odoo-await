41bde0ed6ffc5748e70952283c403a4edca928cd
Charlie Wettlaufer <cwpromo@gmail.com>
2020-09-08 20:02:55 -0400

    better address error logging

---
+++ api/controllers/salesforce/webhook.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Webhook',
+
+
+  description: 'Webhook salesforce.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
